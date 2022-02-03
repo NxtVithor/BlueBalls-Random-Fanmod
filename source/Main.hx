f6d3e8be554b743dc7ea641a702c830c038f47ff
Stilic <lateman-jpeg@outlook.fr>
2022-02-03 07:32:02 +0100

    remove crash handler

--- source/Main.hx
+++ source/Main.hx
@@ -210,7 +210,7 @@ class Main extends Sprite
 
 	function onCrash(e:UncaughtErrorEvent):Void
 	{
-		var errMsg:String = "";
+		var errMsg:String = "\nUncaught Error: " + e.error + "\nPlease report this error to the GitHub page: https://github.com/Stilic/Forever-Modding";
 		var path:String;
 		var callStack:Array<StackItem> = CallStack.exceptionStack(true);
 		var dateNow:String = Date.now().toString();
@@ -231,8 +231,6 @@ class Main extends Sprite
 			}
 		}
 
-		errMsg += "\nUncaught Error: " + e.error + "\nPlease report this error to the GitHub page: https://github.com/Yoshubs/Forever-Engine";
-
 		if (!FileSystem.exists("./crash/"))
 			FileSystem.createDirectory("./crash/");
 
@@ -241,26 +239,7 @@ class Main extends Sprite
 		Sys.println(errMsg);
 		Sys.println("Crash dump saved in " + Path.normalize(path));
 
-		var crashDialoguePath:String = "FE-CrashDialog";
-
-		#if windows
-		crashDialoguePath += ".exe";
-		#end
-
-		if (FileSystem.exists("./" + crashDialoguePath))
-		{
-			Sys.println("Found crash dialog: " + crashDialoguePath);
-
-			#if linux
-			crashDialoguePath = "./" + crashDialoguePath;
-			#end
-			new Process(crashDialoguePath, [path]);
-		}
-		else
-		{
-			Sys.println("No crash dialog found! Making a simple alert instead...");
-			Application.current.window.alert(errMsg, "Error!");
-		}
+		Application.current.window.alert(errMsg, "Error!");
 
 		Sys.exit(1);
 	}
