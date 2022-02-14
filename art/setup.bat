8bcaace7e81072a747dd603446f6360c10c6ac65
Stilic <lateman-jpeg@outlook.fr>
2022-02-14 08:08:26 +0100

    im fat

--- art/setup.bat
+++ art/setup.bat
@@ -1,17 +1,16 @@
 @echo off
 title FNF Setup - Start
-echo Make sure Haxe 4.1.5 and HaxeFlixel is installed (4.1.5 is important)!
+echo Make sure the latest Haxe version and HaxeFlixel is installed!
 echo Press any key to install required libraries.
 pause >nul
 title FNF Setup - Installing libraries
 echo Installing haxelib libraries...
-haxelib install lime 7.8.0
+haxelib install lime
 haxelib install openfl
-haxelib install flixel 4.8.1
+haxelib install flixel
 haxelib install flixel-addons
 haxelib install flixel-ui
 haxelib install hscript
-haxelib install newgrounds
 haxelib run lime setup
 haxelib install flixel-tools
 title FNF Setup - User action required
@@ -29,52 +28,6 @@ pause >nul
 title FNF Setup - Installing libraries
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 cls
-goto UserActions1
-
-:UserActions1
-title FNF Setup - User action required
-set /p menu="Would you like to fix the transition bug? [Y/N]"
-       if %menu%==Y goto FixTransitionBug
-       if %menu%==y goto FixTransitionBug
-       if %menu%==N goto UserActions2
-       if %menu%==n goto UserActions2
-       cls
-
-:UserActions2
-cls
-title FNF Setup - User action required
-set /p menu2="Would you like to automatically make the APIStuff file? [Y/N]"
-       if %menu2%==Y goto APIStuffYes
-       if %menu2%==y goto APIStuffYes
-       if %menu2%==N goto APIStuffNo
-       if %menu2%==n goto APIStuffNo
-       cls
-       
-:APIStuffYes
-rem Stores the APIStuff.hx contents automatically
-cd source
-(
-echo package;
-echo class APIStuff
-echo {
-echo         public static var API:String = "";
-echo         public static var EncKey:String = "";
-echo }
-)>APIStuff.hx
-cd ..
-goto APIStuffNo
-
-
-:APIStuffNo
-cls
-title FNF Setup - User action required
-set /p menu="Would you like to install Visual Studio Community and components? (Necessary to compile/ 5.5GB) [Y/N]"
-       if %menu%==Y goto InstallVSCommunity
-       if %menu%==y goto InstallVSCommunity
-       if %menu%==N goto SkipVSCommunity
-       if %menu%==n goto SkipVSCommunity
-       cls
-
 
 :SkipVSCommunity
 cls
@@ -82,11 +35,6 @@ title FNF Setup - Success
 echo Setup successful. Press any key to exit.
 pause >nul
 exit
-
-:FixTransitionBug
-title FNF Setup - Installing libraries
-haxelib git flixel-addons https://github.com/HaxeFlixel/flixel-addons
-goto UserActions2
 
 :InstallVSCommunity
 title FNF Setup - Installing Visual Studio Community
